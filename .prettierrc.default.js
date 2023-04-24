f1369ebe2f736f83c67e81d870f80b5678033d75
Fourteen <53899166+FourteenD@users.noreply.github.com>
2023-04-24 19:10:11 +0800

    Update .prettierrc.default.js

--- .prettierrc.default.js
+++ .prettierrc.default.js
@@ -1,56 +1,56 @@
 module.exports = {
   // 每行代码的最大长度
-  "printWidth": 80,
+  printWidth: 80,
   // 一个制表符等于几个空格
-  "tabWidth": 2,
+  tabWidth: 2,
   // 是否使用制表符而不是空格
-  "useTabs": false,
+  useTabs: false,
   // 是否在语句末尾添加分号
-  "semi": true,
+  semi: true,
   // 是否使用单引号而不是双引号
-  "singleQuote": false,
+  singleQuote: false,
   // 对象属性是否强制使用引号
-  "quoteProps": "as-needed",
+  quoteProps: "as-needed",
   // 在JSX中是否使用单引号而不是双引号
-  "jsxSingleQuote": false,
+  jsxSingleQuote: false,
   // 数组或对象最后一个元素后是否添加逗号
-  "trailingComma": "es5",
+  trailingComma: "es5",
   // 是否在对象的花括号之间添加空格
-  "bracketSpacing": true,
+  bracketSpacing: true,
   // 在JSX中是否将>放在最后一行的末尾，而不是新起一行
-  "jsxBracketSameLine": false,
+  jsxBracketSameLine: false,
   // 箭头函数参数是否始终使用括号
-  "arrowParens": "always",
+  arrowParens: "always",
   // 是否在文件顶部插入一个特殊的注释，指定这是格式化过的代码
-  "requirePragma": false,
+  requirePragma: false,
   // 是否在文件顶部插入一个特殊的注释，指定这是格式化过的代码
-  "insertPragma": false,
+  insertPragma: false,
   // 是否在Markdown文本中保留换行符
-  "proseWrap": "preserve",
+  proseWrap: "preserve",
   // HTML空格敏感度
-  "htmlWhitespaceSensitivity": "css",
+  htmlWhitespaceSensitivity: "css",
   // 是否缩进Vue文件中的script和style标签
-  "vueIndentScriptAndStyle": false,
+  vueIndentScriptAndStyle: false,
   // 换行符类型
-  "endOfLine": "auto",
+  endOfLine: "auto",
   // 是否格式化嵌入式语言（如HTML中的JS代码）
-  "embeddedLanguageFormatting": "auto",
-  /** 其它示例 */
-  // 插件  
-  "plugins": ["prettier-plugin-packagejson"],
+  embeddedLanguageFormatting: "auto",
+  /** 其它示例 **/
+  // 插件
+  plugins: ["prettier-plugin-packagejson"],
   // 覆盖
-  "overrides": [
+  overrides: [
     {
-      "files": "*.test.js",
-      "options": {
-        "semi": true
-      }
+      files: "*.test.js",
+      options: {
+        semi: true,
+      },
     },
     {
-      "files": ["*.html", "legacy/**/*.js"],
-      "options": {
-        "tabWidth": 4
-      }
-    }
-  ]
-}
+      files: ["*.html", "legacy/**/*.js"],
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
+};
